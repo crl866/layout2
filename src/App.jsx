f3310d8119ee40c02b807e9e1aa2075d90d0bf5c
CarlAnton <capdavid.student@ua.edu.ph>
2025-09-16 11:35:45 +0800

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-export default function App() {
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
   return (
-    <div className="min-h-screen p-4 bg-lime-200 flex flex-col gap-4">
-
-      {/* Top Section */}
-      <div className="flex justify-between gap-4 p-2 border-4 border-blue-600 bg-sky-200">
-        <div className="flex-1 border-4 border-rose-300 bg-orange-200"></div>
-        <div className="flex border-4 border-red-600 bg-rose-200">
-          <div className="flex-1 border-4 border-rose-400"></div>
-          <div className="flex-1 border-4 border-rose-400"></div>
+    <div className="MainContainer">
+      <div className="Section1">
+        <div className="Box1"></div>
+        <div className="Box2">
+          <div className="InnerBox1"></div>
+          <div className="InnerBox2"></div>
         </div>
-        <div className="flex-1 border-4 border-rose-300 bg-orange-200"></div>
+        <div className="Box3"></div>
       </div>
-
-      {/* Middle Section */}
-      <div className="flex gap-4 p-2">
-        <div className="flex-1 border-4 border-rose-700 bg-rose-300 min-h-[200px]"></div>
-        <div className="flex flex-col gap-4 flex-1">
-          <div className="border-4 border-rose-700 bg-rose-300 flex-1"></div>
-          <div className="border-4 border-rose-700 bg-rose-300 flex-1"></div>
+      <div className="Section2">
+        <div className="leftside"></div>
+        <div className="rightside">
+          <div className="Box5"></div>
+          <div className="Box6"></div>
         </div>
       </div>
-
-      {/* Bottom Section */}
-      <div className="flex justify-between gap-4 p-2 border-4 border-blue-600 bg-sky-200">
-        <div className="flex-1 border-4 border-blue-400 bg-indigo-200"></div>
-        <div className="flex-1 border-4 border-rose-400 bg-orange-200"></div>
-        <div className="flex-1 border-4 border-rose-400 bg-orange-200"></div>
-        <div className="flex-1 border-4 border-blue-400 bg-indigo-200"></div>
+      <div className="Section3">
+        <div className="Box7"></div>
+        <div className="Box8"></div>
+        <div className="Box9"></div>
+        <div className="Box10"></div>
       </div>
-
     </div>
-  );
+  )
 }
+
+export default App
